d228e0d1b1e6c47ec6e6d8475004782dff675662
钱梦璋 <qmznj1998@163.com>
2017-11-13 10:44:55 +0800

    Initial Commit

---
+++ Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Calculator
+//
+//  Created by QMZ on 2017/11/13.
+//  Copyright © 2017年 QMZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
