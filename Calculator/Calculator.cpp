e49d770f533dfa7905ab60a320dad457d484f235
QMZ <qmz@QMZdeMacBook-Air.local>
2017-11-29 17:18:14 +0800

    A calculator implementation

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,9 @@
+//
+//  Calculator.cpp
+//  Calculator
+//
+//  Created by QMZ on 2017/11/13.
+//  Copyright © 2017年 QMZ. All rights reserved.
+//
+
+#include "Calculator.hpp"
---
+++ Calculator/Calculator.h
@@ -0,0 +1,14 @@
+//
+//  Calculator.hpp
+//  Calculator
+//
+//  Created by QMZ on 2017/11/13.
+//  Copyright © 2017年 QMZ. All rights reserved.
+//
+
+#ifndef Calculator_hpp
+#define Calculator_hpp
+
+#include <stdio.h>
+
+#endif /* Calculator_hpp */
